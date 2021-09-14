659ecaac2f3660416c3b8e03233b488b6b591a37
GyuYoungCho <rbud613@gmail.com>
2021-09-14 19:40:12 +0900

    S05P21A604-101 add : bookdetail

--- backend_spring/book/src/main/java/com/project/book/controller/BookController.java
+++ backend_spring/book/src/main/java/com/project/book/controller/BookController.java
@@ -17,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,7 +45,7 @@ public class BookController {
 	@ApiOperation(value = "단어를 포함한 book 검색", response = String.class)
 	@GetMapping("/search")
 	public Page<Book> getBookSearchList(@RequestParam("keyword") String keyword ,final Pageable pageable) throws NumberFormatException, ParseException{
-		String url = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=" + key + "&Query=" + keyword + "&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101";
+		String url = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=" + key + "&Query=" + keyword + "&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101";
 		JSONArray jsonArray=JSONParsing(url);
 		SimpleDateFormat dateparse = new SimpleDateFormat("yyyy-mm-dd");
 		List<Book> bookList=new ArrayList<>();
@@ -52,14 +54,14 @@ public class BookController {
             JSONObject data=(JSONObject) jsonArray.get(i);
             
             Book book=new Book(1,
-                    data.get("tile").toString(),
+                    data.get("title").toString(),
                     data.get("link").toString(),
                     data.get("author").toString(),
                     dateparse.parse(data.get("pubDate").toString()),
                     data.get("description").toString(),
                     Integer.parseInt(data.get("priceSales").toString()),
                     Integer.parseInt(data.get("priceStandard").toString()),
-                    Integer.parseInt(data.get("priceStandard").toString()),
+                    Integer.parseInt(data.get("mileage").toString()),
                     data.get("malltype").toString(),
                     data.get("cover").toString(),
                     Integer.parseInt(data.get("categoryId").toString()),
@@ -74,9 +76,35 @@ public class BookController {
 	
 	@ApiOperation(value = "book의 상세 정보", response = String.class)
 	@GetMapping("/detail/{bookId}")
-	public Page<Book> bookdetail(@PathVariable("bookId") int bookId){
-		String url = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=[TTBKey]&itemIdType=ISBN&ItemId=[도서의ISBN]&output=xml&Version=20131101&OptResult=ebookList,usedList,reviewList";
-		return null;
+	public ResponseEntity<Book> bookdetail(@PathVariable("bookId") String bookId) throws NumberFormatException, ParseException{
+		String itemurl = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey="+ key + 
+				"&itemIdType=ISBN&ItemId="+ bookId + "&output=js&Version=20131101";
+		JSONArray jsonArray=JSONParsing(itemurl);
+		
+		if (jsonArray.isEmpty()) return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+		
+		JSONObject data=(JSONObject) jsonArray.get(0);
+		SimpleDateFormat dateparse = new SimpleDateFormat("yyyy-mm-dd");
+		
+		JSONObject subinfodata=(JSONObject) data.get("subInfo");
+		
+		Book book=new Book(1,
+                data.get("title").toString(),
+                data.get("link").toString(),
+                data.get("author").toString(),
+                dateparse.parse(data.get("pubDate").toString()),
+                data.get("description").toString(),
+                Integer.parseInt(data.get("priceSales").toString()),
+                Integer.parseInt(data.get("priceStandard").toString()),
+                Integer.parseInt(data.get("mileage").toString()),
+                data.get("mallType").toString(),
+                data.get("cover").toString(),
+                Integer.parseInt(data.get("categoryId").toString()),
+                data.get("publisher").toString(),
+                Integer.parseInt(subinfodata.get("itemPage").toString())
+        );
+		
+		return new ResponseEntity<Book>(book, HttpStatus.OK);
 	}
 	
 	@ApiOperation(value = "book의 리뷰 목록", response = String.class)
