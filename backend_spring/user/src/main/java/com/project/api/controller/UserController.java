861517392e1d1c846cb9ca9f13ea6ac6581e5306
Hellonaks <krh963852@naver.com>
2021-09-25 17:03:55 +0900

    S05P21A604-111 add : user login

--- backend_spring/user/src/main/java/com/project/api/controller/UserController.java
+++ backend_spring/user/src/main/java/com/project/api/controller/UserController.java
@@ -6,14 +6,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.api.request.UserLoginPostReq;
 import com.project.api.request.UserRegisterPostReq;
 import com.project.api.response.UserLoginPostRes;
 import com.project.api.service.UserService;
 import com.project.common.model.response.BaseResponseBody;
+import com.project.common.util.JwtTokenUtil;
 import com.project.entity.User;
 
 import io.swagger.annotations.Api;
@@ -35,38 +38,33 @@ public class UserController {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
-//	@PutMapping("/login")
-//	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
-//    @ApiResponses({
-//        @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-//        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-//        @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
-//        @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
-//    })
-//	public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginPostReq loginInfo) {
-//		String email = loginInfo.getEmail();
-//		String password = loginInfo.getPassword();
-//		
-//		//이메일로 수정
-//		User user = userService.getUserByEmail(email);
+	@PutMapping("/login")
+	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
+        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
+        @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
+        @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
+    })
+	public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginPostReq loginInfo) {
+		String id = loginInfo.getId();
+		String password = loginInfo.getPassword();
+		
+		//아이디로 수정
+		User user = userService.getUserById(id);
 //		Profile profile = profileService.getProfileByUserId(user.getUserId());
-//		/*
-//		 * 탈퇴한 회원(user status 가 1일때)인 경우 로그인 에러
-//		 */
-//		if(user.getUserStatus() == 1)
-//			return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "탈퇴한 회원입니다.", null));
-//
-//		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
-//		if(passwordEncoder.matches(password, user.getPassword())) {
-//			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-//			log.info("로그인 성공했습니다");
-//			String token = JwtTokenUtil.getToken(email);
-//			userService.changeToken(email, token);
-//			return ResponseEntity.ok(UserLoginPostRes.of(200, profile.getNickname(), token));
-//		}
-//		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-//		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
-//	}
+		
+		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
+		if(passwordEncoder.matches(password, user.getPassword())) {
+			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
+			log.info("로그인 성공했습니다");
+			String token = JwtTokenUtil.getToken(id);
+			userService.changeToken(id, token);
+			return ResponseEntity.ok(UserLoginPostRes.of(200, "어서오세요", token));
+		}
+		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
+		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
+	}
 	
 	@PostMapping("/signup")
 	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.") 
